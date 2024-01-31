c80a71e0815857ef3609617271455e4912045514
juansanmartin251880 <44731633+jd-sanmartin@users.noreply.github.com>
2024-01-30 20:25:47 -0500

    Fixed Seconds format

--- utils/formatYoutubeVideoDuration.ts
+++ utils/formatYoutubeVideoDuration.ts
@@ -3,5 +3,5 @@ export default function(_duration: string) {
   const seconds = duration[duration.length - 1]
   const minutes = duration[duration.length - 2]
   const hours = duration[duration.length - 3]
-  return `${hours ? hours + ':' : ''}${minutes ? minutes + ':' : ''}${seconds}`
+  return `${hours ? hours + ':' : ''}${minutes ? minutes + ':' : '0'}${seconds.length === 2 ? seconds : '0' + seconds}`
 }
